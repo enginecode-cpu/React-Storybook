eec9f526b7330cf1463e4777de42b2c0f4a8e8e7
enginecode-cpu <bassmatics1002@gmail.com>
2021-01-14 18:29:47 +0900

    스펠링 수정 및 버튼 추가

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -7,7 +7,27 @@ export default {
   component: Button,
 };
 
-export const Primary = () => <Button varient="primary">Primary</Button>;
-export const Secondary = () => <Button varient="secondary">Secondary</Button>;
-export const Success = () => <Button varient="success">Success</Button>;
-export const Danger = () => <Button varient="danger">Danger</Button>;
+export const Primary = () => <Button variant="primary">Primary</Button>;
+export const Secondary = () => <Button variant="secondary">Secondary</Button>;
+export const Success = () => <Button variant="success">Success</Button>;
+export const Danger = () => <Button variant="danger">Danger</Button>;
+
+const Templates = (args) => <Button {...args} />;
+
+export const PrimaryA = Templates.bind({});
+PrimaryA.args = {
+  variant: "primary",
+  children: "Primary Args",
+};
+
+export const LongPrimaryA = Templates.bind({});
+LongPrimaryA.args = {
+  ...PrimaryA.args,
+  children: "Long Primary Args",
+};
+
+export const SecondaryA = Templates.bind({});
+SecondaryA.args = {
+  variant: "secondary",
+  children: "Secondary Args",
+};
