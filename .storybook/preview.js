96b7998a0fd74644dac91402ada37d15e54df213
enginecode-cpu <bassmatics1002@gmail.com>
2021-01-18 16:54:42 +0900

    Viewport Addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { addDecorator } from "@storybook/react";
+import { addDecorator, addParameters } from "@storybook/react";
 import { withConsole } from "@storybook/addon-console";
 import { withKnobs } from "@storybook/addon-knobs";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
 addDecorator(withKnobs);
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+});
